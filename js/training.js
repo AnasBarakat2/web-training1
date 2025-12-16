3a7dd12b6625bcd0d71871f3c071106de2414d32
AnasBarakat2 <anasbarakat92@gmail.com>
2025-12-16 19:31:59 +0300

    update

---
+++ js/training.js
@@ -0,0 +1,10 @@
+
+
+document.getElementById('h1').innerHTML='delete msg';//to edit on any thing in html code
+
+document.getElementById('h1').style.color='red' // to edit on style from js
+
+var x =2;
+var y=4;
+
+document.writeln(x**y)
