cb1a9afc098791cadc3165f28cdd6c58cde0e60d
AnasBarakat2 <anasbarakat92@gmail.com>
2025-12-17 20:39:33 +0300

    update

--- js/training.js
+++ js/training.js
@@ -1,10 +1,18 @@
 
 
-document.getElementById('h1').innerHTML='delete msg';//to edit on any thing in html code
+// document.getElementById('h1').innerHTML='delete msg';//to edit on any thing in html code
 
-document.getElementById('h1').style.color='red' // to edit on style from js
+// document.getElementById('h1').style.color='red' // to edit on style from js
 
-var x =2;
-var y=4;
+// var x =2;
+// var y=4;
 
-document.writeln(x**y)
+// document.writeln(x**y)
+
+var element = document.getElementById('ptag')
+element.innerHTML='this text after change!' 
+
+var x = document.getElementsByClassName('htag') // to change many element have same class name
+x[0].innerHTML='hello0'
+x[1].innerHTML='heelo1'
+document.writeln(x.length) //counter how many element have name class x
