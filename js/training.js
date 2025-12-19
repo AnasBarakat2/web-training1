17970636f9315a8a255751dcf9fb4ca03f2ca479
AnasBarakat2 <anasbarakat92@gmail.com>
2025-12-19 19:10:28 +0300

    update

--- js/training.js
+++ js/training.js
@@ -51,3 +51,11 @@ function count(){
    } 
 
 }
+
+
+var themeToggle = document.getElementById('theme');
+ var body =document.body;
+
+themeToggle.addEventListener('click',()=>{
+    body.classList.toggle('dark-mode');
+ })
