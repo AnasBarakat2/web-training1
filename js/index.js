bcb708888d579dfff00aa15c5f349a1de7896f7b
AnasBarakat2 <anasbarakat92@gmail.com>
2026-01-17 20:50:16 +0300

    update

--- js/index.js
+++
@@ -1,6 +0,0 @@
-const button = document.getElementById('colorBtn');
-
-button.addEventListener('click', () => {
-    document.body.style.backgroundColor = 
-        document.body.style.backgroundColor === 'lightblue' ? '#f4f4f9' : 'lightblue';
-});
